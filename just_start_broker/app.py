4f4a397f8287f128ee8dc929f98cf09ade917f31
Ali Ghahraei <aligf94@gmail.com>
2022-08-28 23:31:27 -0500

    Add read schedule route

--- just_start_broker/app.py
+++ just_start_broker/app.py
@@ -1,15 +1,18 @@
-from typing import Protocol
+from typing import Protocol, Type
 
 from fastapi import Depends, FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_422_UNPROCESSABLE_ENTITY
 
 from just_start_broker.file_persistence import get_schedule_accessor
 from just_start_broker.persistence import ScheduleNotExpired, ScheduleNotFoundError
 from just_start_broker.schemas import Schedule
 
-CLIENT_ERRORS = [ScheduleNotExpired, ScheduleNotFoundError]
+CLIENT_ERRORS = {
+    ScheduleNotExpired: HTTP_422_UNPROCESSABLE_ENTITY,
+    ScheduleNotFoundError: HTTP_404_NOT_FOUND,
+}
 app = FastAPI()
 
 
@@ -17,15 +20,21 @@ class ScheduleAccessor(Protocol):
     def create(self, schedule: Schedule) -> None:
         pass
 
+    def read(self) -> Schedule:
+        pass
 
-for error_type in CLIENT_ERRORS:
 
-    @app.exception_handler(error_type)
+def register_handler(handled_type: Type[Exception], status: int) -> None:
+    @app.exception_handler(handled_type)
     async def client_error_handler(_: Request, ex: Exception) -> JSONResponse:
         return JSONResponse(
-            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            status_code=status,
             content=jsonable_encoder({"error": str(ex)}),
         )
+
+
+for error_type, http_status in CLIENT_ERRORS.items():
+    register_handler(error_type, http_status)
 
 
 @app.post("/schedule", response_model=Schedule)
@@ -35,3 +44,10 @@ def create_schedule(
 ) -> Schedule:
     accessor.create(schedule)
     return schedule
+
+
+@app.get("/schedule", response_model=Schedule)
+def read_schedule(
+    accessor: ScheduleAccessor = Depends(get_schedule_accessor),
+) -> Schedule:
+    return accessor.read()
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,9 +5,10 @@ from unittest.mock import Mock
 from just_start_broker.app import app, ScheduleAccessor
 from just_start_broker.file_persistence import get_schedule_accessor
 from just_start_broker.persistence import ScheduleNotExpired, ScheduleNotFoundError
+from just_start_broker.schemas import Event, Schedule
 
-from just_start_broker.schemas import Schedule
-from pytest import fixture, FixtureRequest, mark
+from pytest import fixture, mark
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_422_UNPROCESSABLE_ENTITY
 from starlette.testclient import TestClient
 
 
@@ -19,100 +20,139 @@ def client() -> TestClient:
 class TestSchedule:
     @staticmethod
     @fixture(autouse=True)
-    def accessor(request: FixtureRequest) -> Mock:
+    def accessor() -> Mock:
         mock = Mock(spec_set=ScheduleAccessor)
-        side_effects = {
-            "ScheduleNotExpired": ScheduleNotExpired(datetime(2022, 8, 15)),
-            "ScheduleNotFoundError": ScheduleNotFoundError,
-        }
-        if (param := getattr(request, "param", None)) is not None:
-            mock.create.side_effect = side_effects[param]
         app.dependency_overrides[get_schedule_accessor] = lambda: mock
         return mock
 
     @staticmethod
-    @mark.parametrize(
-        "payload",
-        [
-            {},
-            {"expiration": "2022-08-14T08:00", "events": []},
-            {"expiration": "2022-08-14T08:00", "events": [{}]},
-        ],
-    )
-    def test_create_raises_unprocessable_entity_given_invalid_payload(
-        client: TestClient, payload: dict[str, Any]
-    ) -> None:
-        response = client.post("/schedule", json=payload)
+    @fixture
+    def schedule_dict() -> dict[str, Any]:
+        return {
+            "expiration": "2022-08-15T00:00:00",
+            "events": [
+                {
+                    "type": "Work",
+                    "start": "2022-08-14T03:00:00",
+                    "end": "2022-08-14T04:00:00",
+                }
+            ],
+        }
 
-        assert response.status_code == 422
+    class TestCreate:
+        class TestExceptions:
+            @staticmethod
+            @fixture(autouse=True)
+            def accessor(accessor: Mock) -> Mock:
+                accessor.create.side_effect = ScheduleNotFoundError
+                return accessor
 
-    class TestValidPayload:
-        @staticmethod
-        @fixture
-        def payload() -> dict[str, Any]:
-            return {
-                "expiration": "2022-08-15T00:00:00",
-                "events": [
-                    {
-                        "type": "Work",
-                        "start": "2022-08-14T03:00:00",
-                        "end": "2022-08-14T04:00:00",
-                    }
-                ],
-            }
+            @staticmethod
+            def test_create_returns_expected_status_for_exception(
+                client: TestClient,
+                schedule_dict: dict[str, Any],
+            ) -> None:
+                response = client.post("/schedule", json=schedule_dict)
+
+                assert response.status_code == HTTP_404_NOT_FOUND
+
+            @staticmethod
+            def test_create_shows_expected_message_if_schedule_raises_client_error(
+                client: TestClient,
+                schedule_dict: dict[str, Any],
+            ) -> None:
+                response = client.post("/schedule", json=schedule_dict)
+
+                assert response.json() == {
+                    "error": "Schedule did not exist",
+                }
 
         @staticmethod
         @mark.parametrize(
-            "accessor", ["ScheduleNotExpired", "ScheduleNotFoundError"], indirect=True
+            "payload",
+            [
+                {},
+                {"expiration": "2022-08-14T08:00", "events": []},
+                {"expiration": "2022-08-14T08:00", "events": [{}]},
+            ],
         )
-        def test_create_raises_unprocessable_entity_if_schedule_has_not_expired(
-            client: TestClient, accessor: Mock, payload: dict[str, Any]
+        def test_create_raises_unprocessable_entity_given_invalid_payload(
+            client: TestClient, payload: dict[str, Any]
         ) -> None:
             response = client.post("/schedule", json=payload)
 
             assert response.status_code == 422
 
         @staticmethod
-        @mark.parametrize(
-            "accessor,expected_error",
-            [
-                ["ScheduleNotFoundError", "Schedule did not exist"],
-                [
-                    "ScheduleNotExpired",
-                    "Cannot set a new schedule until 2022-08-15 00:00:00",
-                ],
-            ],
-            indirect=["accessor"],
-        )
-        def test_create_shows_expected_message_if_schedule_raises_client_error(
-            client: TestClient,
-            accessor: Mock,
-            expected_error: str,
-            payload: dict[str, Any],
-        ) -> None:
-            response = client.post("/schedule", json=payload)
-
-            assert response.json() == {
-                "error": expected_error,
-            }
-
-        @staticmethod
         def test_create_schedule_creates_schedule(
             client: TestClient,
             accessor: Mock,
-            payload: dict[str, Any],
+            schedule_dict: dict[str, Any],
             schedule: Schedule,
         ) -> None:
-            client.post("/schedule", json=payload)
+            client.post("/schedule", json=schedule_dict)
 
             accessor.create.assert_called_once_with(schedule)
 
         @staticmethod
-        def test_create_schedule_responds_correctly(
+        def test_create_schedule_returns_ok(
             client: TestClient,
-            payload: dict[str, Any],
+            schedule_dict: dict[str, Any],
         ) -> None:
-            response = client.post("/schedule", json=payload)
+            assert client.post("/schedule", json=schedule_dict).status_code == 200
 
-            assert response.status_code == 200
-            assert response.json() == payload
+        @staticmethod
+        def test_create_schedule_responds_with_schedule(
+            client: TestClient,
+            schedule_dict: dict[str, Any],
+        ) -> None:
+            assert client.post("/schedule", json=schedule_dict).json() == schedule_dict
+
+    class TestRead:
+        class TestExceptions:
+            @staticmethod
+            @fixture(autouse=True)
+            def accessor(accessor: Mock) -> Mock:
+                accessor.read.side_effect = ScheduleNotExpired(datetime(2022, 8, 15))
+                return accessor
+
+            @staticmethod
+            def test_read_returns_expected_status_for_exception(
+                client: TestClient,
+            ) -> None:
+                response = client.get("/schedule")
+
+                assert response.status_code == HTTP_422_UNPROCESSABLE_ENTITY
+
+            @staticmethod
+            def test_read_shows_expected_message_if_schedule_raises_client_error(
+                client: TestClient,
+            ) -> None:
+                response = client.get("/schedule")
+
+                assert response.json() == {
+                    "error": "Cannot set a new schedule until 2022-08-15 00:00:00",
+                }
+
+        class TestWithExistingSchedule:
+            @staticmethod
+            @fixture(autouse=True)
+            def accessor(accessor: Mock) -> Mock:
+                accessor.read.return_value = Schedule(
+                    [Event("Work", datetime(2022, 8, 14, 3), datetime(2022, 8, 14, 4))],
+                    datetime(2022, 8, 15),
+                )
+                return accessor
+
+            @staticmethod
+            def test_read_schedule_responds_with_schedule(
+                client: TestClient,
+                schedule_dict: dict[str, Any],
+            ) -> None:
+                assert client.get("/schedule").json() == schedule_dict
+
+            @staticmethod
+            def test_read_schedule_returns_ok(
+                client: TestClient,
+            ) -> None:
+                assert client.get("/schedule").status_code == 200
